a7bf0af5f0dbb4d7a49b5637893e8edfd542a011
Nicola Solazzo <solazzo.nicola@gmail.com>
2024-01-19 20:48:13 +0100

    Update high_score.py

--- day-5/high_score.py
+++ day-5/high_score.py
@@ -1,1 +1,11 @@
 # high score exercise
+student_scores = [78, 65, 89, 86, 55, 91, 64, 89] # 91 is the highest score
+for n in range(0, len(student_scores)):  # 0, 1, 2, 3, 4, 5, 6, 7
+    student_scores[n] = int(student_scores[n]) # convert string to integer
+
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+
+print(f"The highest score in the class is: {highest_score}")
