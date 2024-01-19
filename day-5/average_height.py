d50a5301d7aa1451e7ea2c937e6cc59ed1227dca
Nicola Solazzo <solazzo.nicola@gmail.com>
2024-01-19 20:38:59 +0100

    Update average_height.py

--- day-5/average_height.py
+++ day-5/average_height.py
@@ -1,1 +1,19 @@
 # average height exercise
+list_of_height = [180, 124, 165, 173, 189, 169, 146]
+
+total_height = 0
+
+for height in list_of_height:
+    total_height += height
+
+print(f"Total height is {total_height}")
+
+number_of_students = 0
+
+for student in list_of_height:
+    number_of_students += 1
+
+print(f"Number of students is {number_of_students}")
+
+average_height = round(total_height / number_of_students)
+print(f"Average height is {average_height}")
